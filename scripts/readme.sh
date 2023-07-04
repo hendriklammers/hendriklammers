f4e2098c90884b1cc5141fa9a14662c3a5a3febc
Hendrik Lammers <lammers@gmail.com>
2023-07-04 20:50:38 +0200

    Add latest articles to readme

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -1,3 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-curl https://www.hendriklammers.com/api/github | jq -r '.introText' > README.md
+json_output=$(curl -s https://www.hendriklammers.com/api/github)
+
+echo $json_output | jq -r '.introText' > README.md
+echo "" >> README.md
+echo "" >> README.md
+echo "## Latest articles" >> README.md
+
+num_articles=$(echo $json_output | jq '.latestArticles | length')
+for (( i=0; i<$num_articles; i++ ))
+do
+  title=$(echo $json_output | jq -r ".latestArticles[$i].title")
+  url=$(echo $json_output | jq -r ".latestArticles[$i].url")
+
+  echo "- [$title]($url)" >> README.md
+done
