30ef5bc11c1962504491e9e4caa68ba97ec69aa9
Hendrik Lammers <lammers@gmail.com>
2023-10-09 08:09:43 +0800

    Use printf instead of echo

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -2,16 +2,14 @@
 
 json_output=$(curl -s https://www.hendriklammers.com/api/github)
 
-echo $json_output | jq -r '.introText' > README.md
-echo "" >> README.md
-echo "" >> README.md
-echo "### Latest articles" >> README.md
+echo "$json_output" | jq -r '.introText' > README.md
+printf "\n\n### Latest articles\n\n" >> README.md
 
-num_articles=$(echo $json_output | jq '.latestArticles | length')
-for (( i=0; i<$num_articles; i++ ))
+num_articles=$(echo "$json_output" | jq '.latestArticles | length')
+for (( i=0; i<num_articles; i++ ))
 do
-  title=$(echo $json_output | jq -r ".latestArticles[$i].title")
-  url=$(echo $json_output | jq -r ".latestArticles[$i].url")
+  title=$(echo "$json_output" | jq -r ".latestArticles[$i].title")
+  url=$(echo "$json_output" | jq -r ".latestArticles[$i].url")
 
   echo "- [$title]($url)" >> README.md
 done
